8d2d2a844cfdaab0ae211092e0e813504e4e0d7f
Alan Dipert <alan@dipert.org>
2013-05-17 01:57:09 -0400

    todoFRP: rm :source-paths and :eval-in from project.clj

--- todoFRP/project.clj
+++ todoFRP/project.clj
@@ -3,10 +3,8 @@
   :description  "TodoFRP using hoplon."
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :source-paths ["src/clj"]
   :plugins      [[tailrecursion/hoplon "0.1.0-SNAPSHOT"]]
   :dependencies [[tailrecursion/hoplon "0.1.0-SNAPSHOT"]]
-  :eval-in      :leiningen
   :hoplon       {:html-out    "resources/public"
                  :cljsc-opts  {:pretty-print   true
                                :optimizations  :whitespace}})
