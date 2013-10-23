e8dfb201e325f1f530634679b8f8ed424d46255e
Micha Niskin <asdf@ubergibson.com>
2013-10-23 03:34:07 -0400

    update for new hoplon

--- todoFRP/doc/index.cljs
+++ todoFRP/doc/index.cljs
@@ -6,86 +6,52 @@
 ;; the terms of this license.
 ;; You must not remove this notice, or any other, from this software.
 
-(ns hoplon-demo-todofrp
-  (:require-macros
-    [tailrecursion.hoplon               :refer [with-frp]]
-    [tailrecursion.javelin              :refer [refer-all cell= with-let]])
+(page index.html
+  (:refer-clojure :exclude [nth])
   (:require
-    tailrecursion.hoplon
-    tailrecursion.javelin
-    [tailrecursion.hoplon.util          :refer [pluralize]]
+    [tailrecursion.hoplon.util          :refer [nth name pluralize route-cell]]
     [tailrecursion.hoplon.storage-atom  :refer [local-storage]]))
-
-(refer-all tailrecursion.hoplon)
-(refer-all tailrecursion.javelin)
 
 ;; internal ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(declare route state active? deleted? completed?)
+(declare route state)
 
-(defn interval [f msec]
-  (.setInterval js/window f msec))
-
-(defn route-cell [msec default]
-  (let [hash  #(.-hash (.-location js/window))] 
-    (with-let [ret (cell (hash))] 
-              (interval #(let [h (hash)] (reset! ret (if (empty? h) default h))) msec))))
-
-(defn- update-state! [todos]
-  (let [grouped (group-by deleted? todos)]
-    (reset! state (into (get grouped false) (get grouped true)))))
-
-(defn- reactive-info [todos i todo]
-  (let [cellval #(cell= (get todo %))]
-    (conj (mapv cellval [:editing :completed :text])
-          (cell= (and (not (deleted? todo))
-                      (or (= "#/" route)
-                          (and (= "#/active" route) (active? todo))
-                          (and (= "#/completed" route) (completed? todo))))))))
-
-(def deleted?     #(empty? (:text (if (map? %) % (nth @state %)))))
-(def completed?   #(:completed (if (map? %) % (nth @state %))))
-(def active?      #(and (not (deleted? %)) (not (completed? %))))
-(def clear        #(assoc % :editing false :completed false :text ""))
-(def dfl-state    (vec (repeat 50 {:editing false :completed true :text ""})))
+(defn reactive-info [todos i todo]
+  [(cell= (:editing   todo))
+   (cell= (:completed todo))
+   (cell= (:text      todo))
+   (cell= (and (not (empty? (:text todo)))
+               (or (= "#/" route)
+                   (and (= "#/active" route) (not (:completed todo)))
+                   (and (= "#/completed" route) (:completed todo)))))])
 
 ;; public ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(def state        (local-storage (cell dfl-state) ::store))
+(def state        (local-storage (-> 50 (repeat nil) vec cell) ::store))
 (def route        (route-cell 50 "#/"))
-(def editing-new  (cell ""))
-(def live-ones    (cell= (filter (complement deleted?) state)))
-(def completed    (cell= (filter completed? live-ones)))
-(def active       (cell= (filter (complement completed?) live-ones)))
+(def completed    (cell= (filter :completed state)))
+(def active       (cell= (remove :completed state)))
 (def plural-item  (cell= (pluralize "item" (count active))))
 (def loop-todos   (thing-looper state reactive-info))
 
-(defn new-item! [text]
-  (if (not (empty? text))
-    (update-state! (into [(assoc (clear (peek @state)) :text text)] (pop @state)))))
+(def todo         #(hash-map :editing false :completed false :text %))
+(def update!      #(swap! state (fn [x] (vec (apply %1 (vec (filter map? x)) %&)))))
+(def new!         #(when (not (empty? %)) (update! (partial into [(todo %)]))))
+(def editing!     #(update! (partial map-indexed (fn [i v] (assoc v :editing (if (= i %1) %2 false))))))
+(def done!        #(update! assoc-in [%1 :completed] %2))
+(def all-done!    #(update! (partial map (fn [x] (assoc x :completed %)))))
+(def clear-done!  (fn [& _] (update! (partial remove :completed))))
+(def destroy!     #(update! (fn [x] (concat (take % x) (drop (inc %) x)))))
+(def text!        #(if (empty? %2) (destroy! %1) (update! assoc-in [%1 :text] %2)))
 
-(defn set-editing! [i v]
-  (update-state! (-> (mapv #(assoc % :editing false) @state)
-                   (assoc-in [i :editing] v))))
-
-(defn set-completed! [i v]
-  (update-state! (assoc-in @state [i :completed] v)))
-
-(defn set-all-completed! [v]
-  (update-state! (mapv #(assoc % :completed v) @state)))
-
-(defn set-text! [i v]
-  (update-state! (assoc-in (vec @state) [i :text] v)))
-
-(defn clear-completed! [& _]
-  (update-state! (mapv #(if (completed? %) (clear %) %) @state)))
+;; page ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (html
   {:lang "en"}
   (head
     (meta {:charset "utf-8"})
     (meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"})
-    (title ($text "Hoplon • TodoMVC"))
+    (title "Hoplon • TodoMVC")
     (link {:rel "stylesheet" :href "../assets/base.css"}))
   (body
     (with-frp
@@ -96,56 +62,55 @@
             {:id "header"}
             (h1 "todos")
             (form
-              {:on-submit [#(do (new-item! @editing-new) (reset! editing-new ""))]}
+              {:on-submit [#(do (new! ~:new-todo) (do! ~@:new-todo :value ""))]}
               (input
-                {:type        "text"
-                 :id          "new-todo"
+                {:id          "new-todo"
+                 :type        "text"
                  :placeholder "What needs to be done?"
                  :autofocus   "autofocus"
-                 :do-value    [editing-new]
-                 :on-keypress [#(reset! editing-new ~:new-todo)]
-                 :on-focusout [#(reset! editing-new "")]})))
+                 :on-focusout [#(do! ~@:new-todo :value "")]})))
           (section
-            {:id "main"
-             :do-class [:hidden (and (empty? active) (empty? completed))]}
+            {:id        "main"
+             :do-toggle [(not (and (empty? active) (empty? completed)))]}
             (input
-              {:type      "checkbox"
-               :id        "toggle-all"
+              {:id        "toggle-all"
+               :type      "checkbox"
                :do-attr   [:checked (empty? active)]
-               :on-click  [#(set-all-completed! (value! (.-target %)))]})
+               :on-click  [#(all-done! ~:toggle-all)]})
             (label {:for "toggle-all"} "Mark all as complete")
             (ul
-              {:id "todo-list"
-               :loop [loop-todos idx is-editing is-completed text is-visible]}
+              {:id   "todo-list"
+               :loop [loop-todos i edit? done? text show? complete# edit#]}
               (li
-                {:do-class [:completed is-completed
-                            :hidden (not is-visible)
-                            :editing is-editing]}
+                {:do-class  [:completed done? :editing edit?]
+                 :do-toggle [show?]}
                 (div
                   {:class       "view"
-                   :on-dblclick [#(set-editing! idx true)]}
+                   :on-dblclick [#(editing! i true)]}
                   (input
-                    {:type      "checkbox"
+                    {:id        complete#
+                     :type      "checkbox"
                      :class     "toggle"
-                     :do-attr   [:checked is-completed]
-                     :on-click  [#(set-completed! idx (value! (.-target %)))]})
+                     :do-attr   [:checked done?]
+                     :on-click  [#(done! i ~complete#)]})
                   (label "~{text}")
                   (button
                     {:type      "submit"
                      :class     "destroy"
-                     :on-click  [#(set-text! idx "")]}))
+                     :on-click  [#(destroy! i)]}))
                 (form
-                  {:on-submit [#(set-editing! idx false)]}
+                  {:on-submit [#(editing! i false)]}
                   (input
-                    {:type        "text"
+                    {:id          edit#
+                     :type        "text"
                      :class       "edit"
                      :do-value    [text]
-                     :do-focus    [is-editing]
-                     :on-focusout [#(set-editing! idx false)]
-                     :on-change   [#(set-text! idx (value! (.-target %)))]})))))
+                     :do-focus    [edit?]
+                     :on-focusout [#(editing! i false)]
+                     :on-change   [#(text! i ~edit#)]})))))
           (footer
             {:id        "footer"
-             :do-class  [:hidden (and (empty? active) (empty? completed))]}
+             :do-toggle [(not (and (empty? active) (empty? completed)))]}
             (span
               {:id "todo-count"}
               (strong "~(count active) ")
@@ -158,7 +123,7 @@
             (button
               {:type      "submit"
                :id        "clear-completed"
-               :on-click  [#(clear-completed!)]}
+               :on-click  [#(clear-done!)]}
               "Clear completed (~(count completed))")))
         (footer
           {:id "info"}
