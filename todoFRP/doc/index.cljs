de30271826eaa45ebdfe3a8b4f72c70205dee153
Micha Niskin <asdf@ubergibson.com>
2013-12-22 17:48:25 -0500

    add todofrp example using HTML syntax instead of sexps

--- todoFRP/doc/index.cljs
+++ todoFRP/doc/index.cljs
@@ -6,7 +6,7 @@
 ;; the terms of this license.
 ;; You must not remove this notice, or any other, from this software.
 
-(page index.html
+(page "index.html"
   (:refer-clojure :exclude [nth])
   (:require
     [tailrecursion.hoplon.util          :refer [nth name pluralize route-cell]]
