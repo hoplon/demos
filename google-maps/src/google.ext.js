621dfd6879126ae698933a0ab2b97eb0f052db2f
Micha Niskin <micha.niskin@gmail.com>
2014-05-27 09:20:19 -0400

    add externs file for google maps api

---
+++ google-maps/src/google.ext.js
@@ -0,0 +1,14 @@
+var google = {};
+
+google.load = function() {};
+
+google.maps = {};
+
+google.maps.Map = function() {};
+
+google.maps.LatLng = function() {};
+
+google.maps.Marker = function() {};
+
+google.maps.Marker.prototype.setMap = function() {};
+google.maps.Marker.prototype.setPosition = function() {};
