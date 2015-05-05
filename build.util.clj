c6b0bab0a59274816b39f9b15f9de71cd6d6295e
mynomoto <mynomoto@gmail.com>
2015-05-04 22:50:15 -0300

    Convert async-webinar to Hoplon 6

--- build.util.clj
+++ build.util.clj
@@ -1,12 +1,12 @@
 (ns build.util
   (:require
-   [clojure.java.io :as io]))
+    [clojure.java.io :as io]))
 
 (defn deps []
   (->> "deps"
-    io/file
-    file-seq
-    (filter #(.isFile %))
-    (sort-by #(.getName %))
-    (map (comp read-string slurp))
-    (reduce into [])))
+       io/file
+       file-seq
+       (filter #(.isFile %))
+       (sort-by #(.getName %))
+       (map (comp read-string slurp))
+       (reduce into [])))
