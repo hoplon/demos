ac49acae7652bcbd7cef00ac1bb3655a3f9435de
mynomoto <mynomoto@gmail.com>
2023-04-02 23:29:13 -0300

    Update counters to use shadow-cljs

---
+++ counters/src/demo/counter.cljs
@@ -0,0 +1,49 @@
+(ns demo.counter
+  (:require
+    [hoplon.core :as h]
+    [hoplon.goog]
+    [javelin.core :as j]))
+
+(h/defelem swap-button
+  [{:keys [state func] :or {func identity} :as attr} kids]
+  (let [attr (dissoc attr :state :func)]
+    (h/button (assoc attr :click #(swap! state func))
+      kids)))
+
+(h/defelem counter
+  [attr _]
+  (let [my-count (j/cell 0)]
+    (h/div attr
+      (h/label (h/text "~{my-count}"))
+      (swap-button :state my-count :func inc "+")
+      (swap-button :state my-count :func dec "-"))))
+
+(h/defelem counters
+  [{:keys [size] :or {size 10} :as attr} _]
+  (let [last-clicked (j/cell nil)
+        attr         (dissoc attr :size)]
+    (h/div attr
+      (h/h1 "A Counting Widget!")
+      (h/p {:toggle last-clicked}
+        (h/text "Last clicked item was ~{last-clicked}"))
+      (h/loop-tpl :bindings [i (j/cell= (range 0 size))]
+        (counter :click #(reset! last-clicked @i))))))
+
+(defn mount-components []
+  (.replaceWith (.getElementById js/document "app")
+    (h/div :id "app"
+      (counters :size 10)
+      (h/p (h/a :href
+             "https://github.com/tailrecursion/hoplon-demos/blob/master/counters/src/index.cljs.hl" "Source code"))
+      (h/p "Inspired by " (h/a :href "https://github.com/swannodette/om/tree/master/examples/counters" "the Om demo of the same name.")))))
+
+(defn start []
+  (mount-components)
+  (js/console.log "Starting..."))
+
+(defn stop []
+  (js/console.log "Stopping..."))
+
+(defn init []
+  (js/console.log "Initializing...")
+  (start))
