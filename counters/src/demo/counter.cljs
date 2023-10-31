32755a9adb0f3773b5e57267940cdb3018c57a02
mynomoto <mynomoto@gmail.com>
2023-10-31 17:18:15 -0300

    Fix link, add cpcache to gitignore

--- counters/src/demo/counter.cljs
+++ counters/src/demo/counter.cljs
@@ -32,8 +32,7 @@
 (defn mount-components []
   (.replaceChildren (.getElementById js/document "app")
     (counters :size 10)
-    (h/p (h/a :href
-           "https://github.com/tailrecursion/hoplon-demos/blob/master/counters/src/index.cljs.hl" "Source code"))
+    (h/p (h/a :href "https://github.com/hoplon/demos/blob/master/counters/src/demo/counter.cljs" "Source code"))
     (h/p "Inspired by " (h/a :href "https://github.com/swannodette/om/tree/master/examples/counters" "the Om demo of the same name."))))
 
 (defn start []
